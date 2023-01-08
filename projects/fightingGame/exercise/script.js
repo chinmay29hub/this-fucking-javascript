ac8b1730e9b4c0432327810461a9c4f22f5f15ab
chinmay29hub <chinmaysonawane57@gmail.com>
2023-01-08 16:47:33 +0530

    fighting game

--- projects/fightingGame/exercise/script.js
+++ projects/fightingGame/exercise/script.js
@@ -44,8 +44,17 @@ let p2HealthDiv = document.getElementById('p2Health')
 // ** Check if either players health is  0 and if it is, then update isOver to true **
 const updateGame = (p1,p2,gameState) => {
   // Update the DOM with the names and the latest health of players
-
+  p1NameDiv.innerText = p1.name
+  p2NameDiv.innerText = p2.name
+  p1HealthDiv.innerText = p1.health
+  p2HealthDiv.innerText = p2.health
   // Condition IF either player health is <= 0 then set isOver to true and declareWinner
+  if (p1.health <= 0 || p2.health <= 0) {
+    game.isOver = true
+    gameState = game.isOver
+    resultDiv.innerText = game.declareWinner(gameState, p1, p2)
+    return gameState
+  }
 
 }
 
@@ -64,25 +73,31 @@ class Player {
   strike (player, enemy, attackDmg) {
     
     // Get random number between 1 - 10 and that is damageAmount
+    const damageAmount = Math.ceil(Math.random() * attackDmg)
 
     // Subtract the enemy health with the damageAmount
+    enemy.health -= damageAmount
 
     //  Update the game and DOM with updateGame()
+    updateGame(p1, p2, game.isOver)
 
     //  Return a message of 'player name attacks enemy name for damageAmount'
-
+    return `${player.name} attacks ${enemy.name} for ${damageAmount} damage`
   }
   // ** Heal the player for random number from  1 to 5 **
   heal (player) {
     
     // Get random number between 1 - 5 and store that in hpAmount
+    const hpAmount = Math.ceil(Math.random() * 5)
 
     // Add hpAmount to players health
+    player.health += hpAmount
 
     //  Update the game and DOM with updateGame()
+    updateGame(p1, p2, game.isOver)
 
     //  Return a message of 'player name heals for hpAmount HP'
-
+    return `${player.name} heals for ${hpAmount} HP`
   }
 }
 
@@ -98,53 +113,75 @@ class Game {
   declareWinner(isOver,p1, p2) {
     
     // Create a message variable that will hold a message based on the condition
-
+    let message = "TIE"
     // If isOver is true AND p1 health is <= 0 then update message variable  to 'p1 WINS!'
-
+    if (isOver == true && p1.health <= 0) {
+      message = `${p2.name} wins!!!`
     // Else if isOver is true AND p2 health is <= 0 then update message variable  to 'p2 WINS!'
+    } else if (isOver == true && p2.health) {
+      message = `${p1.name} wins!!!`
+    }
+    
     // Play victory sound
+    document.getElementById("victory").play()
 
     // Return message variable 
-
+    return message
   }
 
   // ** Reset the players health back to it's original state and isOver to FALSE **
   reset(p1,p2) {
     // set p1 health and p2 health back to 100 and isOver back to false and clear resultDiv.innerText and don't forget to updateGame()
-
+    p1.health = 100
+    p2.health = 100
+    this.isOver = false
+    resultDiv.innerText = ""
+    updateGame(p1, p2, this.isOver)
   }
   
   // ** Simulates the whole match untill one player runs out of health **
   play(p1, p2) {
     // Reset to make sure player health is back to full before starting
-
+    this.reset(p1,p2)
     // Make sure the players take turns untill isOver is TRUE
     while (!this.isOver) {
       //Make sure both players get strike() and heal() once each loop
+      p1.strike(p1, p2, p1.attackDmg)
+      p2.heal(p2)
+      p2.strike(p2, p1, p2.attackDmg)
+      p1.heal(p1)
     }
     // Once isOver is TRUE run the declareWinner() method 
+    return this.declareWinner(this.isOver, p1, p2)
     
   }
 
 }
 
 // ** Create 2 players using the player class **
-
+const player1 = new Player("Chinmay", 100, 10)
+const player2 = new Player("JavaScript", 100, 10)
+// console.log(player1)
 
 // ** Save original Player Data into a variable in order to reset **
-let p1;
-let p2;
+let p1 = player1
+let p2 = player2
 
 // ** Create the game object from the Game class **
+const game = new Game()
+// console.log(game)
 
 // ** Intialize the game by calling updateGame() **
-
+updateGame(p1, p2, game.isOver)
 
 // ** Save intial isOver from the game object inside this variable **
 let gameState;
 
 
 // ** Add a click listener to the simulate button that runs the play() method on click and pass in the players **
+playButton.onclick = () => {
+  result.innerText = game.play(p1, p2)
+}
 
 
 // Add functionality where players can press a button to attack OR heal
@@ -152,16 +189,25 @@ let gameState;
 // ** Player 1 Controls **
 document.addEventListener('keydown', function(e) {
   // if you press Q AND the enemy health is greater than 0 AND isOver is still false then strike()
-
-    // After striking then play attack sound
+    if (e.key == "q" && p2.health > 0 && game.isOver == false) {
+      p1.strike(p1, p2, p1.attackDmg)
+      // After striking then play attack sound
+      document.getElementById("p1attack").play()
+    }
+    
 
 });
 
 document.addEventListener('keydown', function(e) {
   
-  // if you press a AND the player health is greater than 0 AND isOver is still false then strike()
-
+  // if you press a AND the player health is greater than 0 AND isOver is still false then heal()
+  if (e.key == "a" && p2.health > 0) {
+    p1.heal(p1)
     // After healing then play heal sound
+    document.getElementById("p1heal").play()
+  }
+
+    
 
 });
 
@@ -169,15 +215,26 @@ document.addEventListener('keydown', function(e) {
 document.addEventListener('keydown', function(e) {
   
   // if you press p AND enemy health is greater than 0 AND isOver is still false then stike()
-
+  if (e.key == 'p' && p1.health > 0 && game.isOver == false) {
+    p2.strike(p2, p1, p2.attackDmg)
     // After striking then play attack sound
+    document.getElementById("p2attack").play()
+  }
+
+    
 
 });
 
 document.addEventListener('keydown', function(e) {
   // if you press l AND the player health is greater than 0 AND isOver is still false then heal()
 
+  if (e.key == "l" && p1.health > 0) {
+    p2.heal(p2)
     // After healing then play heal sound
+    document.getElementById("p2heal").play()
+  }
+
+    
 
 });
 
